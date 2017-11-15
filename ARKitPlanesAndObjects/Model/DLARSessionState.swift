b957a4ea0a9825246e148f464cb63a4080ed4f84
Ignacio Nieto <contact@digitalleaves.com>
2017-11-15 17:07:04 +0200

    Finished coffee mugs placement

---
+++ ARKitPlanesAndObjects/Model/DLARSessionState.swift
@@ -0,0 +1,9 @@
+//
+//  DLARSessionState.swift
+//  ARKitPlanesAndObjects
+//
+//  Created by Ignacio Nieto Carvajal on 13/11/2017.
+//  Copyright © 2017 Digital Leaves. All rights reserved.
+//
+
+import Foundation
